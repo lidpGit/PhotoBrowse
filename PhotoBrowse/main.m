c9271e3c333c5862a2df4a5e59546cb206fe2c81
lidpGit <lidpzhaozj@163.com>
2016-05-24 11:59:50 +0800

    Initial Commit

---
+++ PhotoBrowse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoBrowse
+//
+//  Created by lidp on 16/5/24.
+//  Copyright © 2016年 lidp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
